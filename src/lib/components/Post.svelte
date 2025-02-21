5e54cc7db14e8c94d1fb8ad616f74f16f99efec1
ikerCaamano <ikercapi08@gmail.com>
2025-02-22 00:04:55 +0100

    Base del Post

---
+++ src/lib/components/Post.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    let {tag , desc} = $props();
+</script>
+
+<style>
+    styleTag{
+
+    }
+
+    styleDesc{
+        
+    }
+</style>
+
+<styleTag>  {tag}  </styleTag>
+<styleDesc> {desc} </styleDesc>
