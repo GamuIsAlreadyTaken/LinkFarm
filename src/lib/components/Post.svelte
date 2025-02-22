ce58275ae8556d7036b68203909fcb9871ca3aee
ikerCaamano <ikercapi08@gmail.com>
2025-02-22 01:37:24 +0100

    Post le falta estilo

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -1,58 +1,34 @@
 <script lang='ts'>
-    let tag = "Título de Prueba";
-    let desc = [
-        "Elemento 1 con <a href='https://ejemplo.com'>enlace</a>", 
-        "Elemento 2 con <a href='https://ejemplo.com'>enlace</a>", 
-        "Elemento 3 con <a href='https://ejemplo.com'>enlace</a>", 
-        "Elemento 4 con <a href='https://ejemplo.com'>enlace</a>"
-    ];
+	import type { Post } from '$lib/types.ts';
+
+    let post:Post = $props();
 </script>
 
 <style>
-    styleTag {
+    .tag {
         font-size: 24px;
         font-weight: bold;
         color: white;
         background-color: #ff5733;
-        padding: 15px;
-        border-radius: 12px;
-        display: inline-block;
-        text-align: center;
-        box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.2);
-    }
-
-    styleDesc {
-        display: flex;
-        flex-direction: column;
-        gap: 15px;
-        margin-top: 20px;
-    }
-
-    styleDesc div {
-        font-size: 20px;
-        padding: 10px 15px;
-        background: linear-gradient(45deg, #f39c12, #d35400);
+        padding: 10px;
         border-radius: 8px;
-        text-align: center;
-        box-shadow: 3px 3px 8px rgba(0, 0, 0, 0.2);
+        display: inline-block;
     }
 
-    styleDesc div a {
+    .des {
         text-decoration: none;
-        color: #fff;
-        font-weight: bold;
-    }
-
-    styleDesc div:hover {
-        background: linear-gradient(45deg, #d35400, #e74c3c);
-        transform: scale(1.05);
+        color: #333;
+        font-size: 18px;
+        padding: 5px 10px;
+        background: #f1c40f;
+        border-radius: 5px;
+        transition: background 0.3s ease;
     }
 </style>
 
-<styleTag>  {tag}  </styleTag>
-
-<styleDesc>
-    {#each desc as des}
-    <div>{@html des}</div>
+<h2 class="tag">  {post.tag.name}  </h2>
+<div> 
+    {#each post.resources as des}
+        <p class="des">{@html des.description}</p>  
     {/each}
-</styleDesc>
+</div>
