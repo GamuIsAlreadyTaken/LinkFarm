f3368064bf0da2ff9d7ca5d668d70c05a0ff22e2
ikerCaamano <ikercapi08@gmail.com>
2025-02-22 04:06:22 +0100

    Logeame esta porsi

---
+++ src/lib/components/login.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+    import { getUserProfile } from '$lib/database';
+	import type { UserProfile } from '$lib/types.ts';
+
+    let {user}: {user: UserProfile} = $props();
+    let login = $state("");
+    function checkuser(){
+        
+    if( getUserProfile(login) != undefined){
+        //Cambiar al buscador
+     }
+    }
+</script>
+
+<input
+      type="text"
+      class="search-input"
+      bind:value={login}
+      placeholder="Buscar..."
+      onkeydown={checkuser}
+    />
