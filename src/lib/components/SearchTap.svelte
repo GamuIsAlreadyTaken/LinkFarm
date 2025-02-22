4da15b10e30125dd955d033aa9aa669b725cf9b2
Sergio Regueiro Martinez <sergioregueiro12@gmail.com>
2025-02-22 04:24:42 +0100

    Casi terminado buscar

---
+++ src/lib/components/SearchTap.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import SearchBar from '$lib/components/TagSearcher.svelte';
+  import { listUserProfile, filters } from '$lib/database';
+  import type { Tag, UserProfile } from '$lib/types';
+  import Profile from './Profile.svelte';
+
+  let searchValue: Tag | null = null;
+  let users: UserProfile[] = [];
+
+  const handleSearchSubmit = (event: KeyboardEvent) => {
+    if (event.key === 'Enter' && searchValue) {
+      users = listUserProfile(filters.postHasTag(searchValue)); // Asegúrate de que `listUserProfile` devuelva un valor
+    }
+  };
+
+  const handleUserClick = (user: UserProfile) => {
+    //Cambiar al perfil
+
+  };
+</script>
+
+<main>
+  <SearchBar bind:value={searchValue} onsubmit={handleSearchSubmit} />
+
+  <h2>Usuarios</h2>
+  <ul>
+    {#each users as user}
+      <li>
+        <button on:click={() => handleUserClick(user)} class="user-card">
+          <h3>{user.name}</h3>
+          <p>{user.contactData}</p>
+        </button>
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+    padding: 20px;
+  }
+
+  h2 {
+    margin-top: 20px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+  }
+
+  li {
+    margin-bottom: 10px;
+  }
+
+  .user-card {
+    background: #fff;
+    border: 2px solid #ccc;
+    padding: 10px;
+    cursor: pointer;
+    display: block;
+    margin: 5px 0;
+    transition: background-color 0.3s ease;
+  }
+
+  .user-card:hover {
+    background-color: #f0f0f0;
+  }
+
+  h3 {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+
+  p {
+    font-size: 1rem;
+    color: #555;
+  }
+</style>
